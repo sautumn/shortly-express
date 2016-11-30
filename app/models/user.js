2d53b8264ccb85f3b2b8f34bc7c7f7eab242e18c
Autumn Smith <autumn07@gmail.com>
2016-11-30 11:51:16 -0800

    changed hashing to sync to fix heroku error

--- app/models/user.js
+++ app/models/user.js
@@ -10,13 +10,15 @@ var User = db.Model.extend({
   defaults: {},
 
   initialize: function() {
-    var context = this;
+    // var context = this;
     var password = this.get('password');
-    bcrypt.genSalt(5, function(err, salt) {
-      bcrypt.hash(password, salt, function(err, hash) {
-        context.set('password', hash);
-      });
-    });
+    // bcrypt.genSalt(5, function(err, salt) {
+    //   bcrypt.hash(password, salt, function(err, hash) {
+    //     context.set('password', hash);
+    //   });
+    // });
+    var hash = bcrypt.hashSync(password);
+    this.set('password',hash);
     // this.on('creating', function(model, attrs, options) {
     // });
   }
